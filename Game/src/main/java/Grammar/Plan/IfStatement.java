fbdf16a9c30f5c72c9b1788cd39240ef2d6f7b5f
Peerawich Rattanahiran <TailBeast113@gmail.com>
2024-01-28 13:28:25 +0700

    do something

--- Game/src/main/java/Grammar/Plan/IfStatement.java
+++ Game/src/main/java/Grammar/Plan/IfStatement.java
@@ -4,11 +4,17 @@ import Grammar.Expression.Expression;
 import Grammar.Plan.Plan;
 
 public class IfStatement extends Plan {
-    private Expression expr
-    private Statement s1
-    private Statement s2
+    private Expression expr;
+    private Statement s1;
+    private Statement s2;
 
-    public IfStatement(Expression expr, Statement s1, Statement s2)
-    public void eval(Player p, Territory t)
+    public IfStatement(Expression expr, Statement s1, Statement s2){
+        this.expr = expr;
+        this.s1 = s1;
+        this.s2 = s2;
+    }
+    public void eval(Player p, Territory t){
+
+    }
 }
 
--- Game/src/main/java/Grammar/Plan/InfoExpression.java
+++ Game/src/main/java/Grammar/Plan/InfoExpression.java
@@ -1,7 +1,6 @@
 package Grammar.Plan;
 
-public record InfoExpression(String command, String dir)
-        implements Expr {
-    public int eval(Map<String, Integer> bindings)
+public record InfoExpression(String command, String dir) implements Expr {
+    public int eval(Map<String, Integer> bindings){}
 }
 
