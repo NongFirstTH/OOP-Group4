4ca5fd81da2fa21cc8a8d33bbad4faa69fdf6393
Peerawich Rattanahiran <TailBeast113@gmail.com>
2024-02-18 14:42:39 +0700

    change newGame1P parameter again

--- src/test/java/GamePlay/GameFactory.java
+++ src/test/java/GamePlay/GameFactory.java
@@ -10,8 +10,8 @@ public class GameFactory {
     }
 
     public Game newGame1P(int row, int col) throws SyntaxError, EvalError {
-        Game g = new Game(row, col);
-        g.addPlayerToTestOnly("p1" ,5, 5, null);
+        Game g = new Game(10, 10);
+        g.addPlayerToTestOnly("p1" ,row, col, null);
         return g;
     }
 
