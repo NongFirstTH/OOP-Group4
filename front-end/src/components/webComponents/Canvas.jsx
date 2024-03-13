4a82fbb9efb10d2626edbf008fceea70fa5d05ff
NongFirstTH <TanabodeeChannel@gmail.com>
2024-03-13 15:06:16 +0700

    Change Canvas.jsx from React.Component to function Canvas with props

--- front-end/src/components/webComponents/Canvas.jsx
+++ front-end/src/components/webComponents/Canvas.jsx
@@ -1,178 +1,141 @@
-import React, { createRef, useEffect } from "react";
+import React, { createRef, useEffect, useState } from "react";
 import "./styles/App.css";
+import { useAppSelector } from "../../store/hooks";
+import { selectConfig } from "../../store/Slices/configSlice";
 
-export default class Canvas extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hexSize: 40,
-      hexOrigin: { x: 0, y: 0 },
-      rows: this.props.mapArray.length,
-      cols: 15,
-      currentHex: { currow: this.props.currow, curcol: this.props.curcol },
-      playerName: 'A',
-    };
-    this.canvasRef = createRef();
-  }
-  componentDidMount = () => {
-    const canvasHex = this.canvasRef.current;
-    // this.drawHex(canvasHex, { x: 50, y: 50 });
-    // console.log(this.props.mapArray);
-    this.drawHexes(canvasHex);
+
+export default function Canvas(props) {
+    const [hexSize, setHexSize] = useState(40);
+    
+    const configState = useAppSelector(selectConfig);
+  const [rows, setRows] = useState(configState.m);
+  const [cols, setCols] = useState(configState.n);
+  const [currentHex, setCurrentHex] = useState({currow: props.currow,curcol: props.curcol,});
+  const [playerName, setPlayerName] = useState("A");
+  const canvasRef = createRef();
+
+  const { width, height } = calculateCanvasSize();
+  useEffect(() => {
+      if (props.mapArray) {
+          const canvasHex = canvasRef.current;
+          drawHexes(canvasHex);
+          console.log(props.mapArray);
+        // console.log(canvasHex);
+        // console.log(cols);
+        // console.log(canvasWidth);
+    }
+  }, [props.mapArray, currentHex]);
+
+  function calculateCanvasSize() {
+    const { hexWidth, hexHeight, vertDist, horizDist } = getHexParameters();
+    const newWidth = cols * horizDist + hexWidth / 2 + 10;
+    const newHeight = rows * vertDist + hexHeight + 20;
+    return { width: newWidth, height: newHeight };
   };
 
-  calculateCanvasSize() {
-    const { hexWidth, hexHeight, vertDist, horizDist } =
-      this.getHexParameters();
-    const { rows, cols } = this.state;
-    const width = cols * horizDist + hexWidth / 2 + 10;
-    const height = rows * vertDist + hexHeight + 20;
-    return { width, height };
-  }
-
-  evenq_to_axial(hex) {
+  const evenq_to_axial = (hex) => {
     const q = hex.col;
     const r = hex.row - (q - (q & 1)) / 2;
-    return this.Hex(q, r);
-  }
+    return Hex(q, r);
+  };
 
-  drawHexes(canvas) {
-    const { hexWidth, hexHeight, vertDist, horizDist } =
-      this.getHexParameters();
-    const { width, height } = this.props;
-    const hexOrigin = {
-      x: hexWidth / 2 + horizDist,
-      y: vertDist + 10,
-    };
-    this.setState({ hexOrigin });
-    // for (let r = 1; r <= this.state.rows; r++) {
-    //   for (let q = 1; q <= this.state.cols; q++) {
-      this.props.mapArray.map((innerArray, q) => {
-        innerArray.map((element,r) => {
-        const evenQCoord = { col: q+1, row: r+1};
-        const axialCoord = this.evenq_to_axial(evenQCoord);
-        let center = this.hexToPixel(axialCoord);
-        if (
-          center.x > hexWidth / 2 &&
-          center.x < width - hexWidth &&
-          center.y > hexHeight / 2 &&
-          center.y < height - hexHeight
-          ) {
-            // console.log(this.props.mapArray[r - 1][q - 1].element);
-          console.log(this.state.playerName);
-          this.drawHex(
-            canvas,
-            center,
-            this.Hex(q+1, r+1),
-            element
-          );
-          if (element.player !== null)
-            this.drawHexCoordinates(
-              canvas,
-              center,
-              this.Hex(q+1, r+1),
-              element
-            );
-        }
-      })
-    })
-  }
+  const drawHexes = (canvas) => {
+    console.log("draw")
+    props.mapArray.map((innerArray, r) => {
+      innerArray.map((element, q) => {
+        const evenQCoord = { col: q + 1, row: r + 1 };
+        const axialCoord = evenq_to_axial(evenQCoord);
+        let center = hexToPixel(axialCoord);
+        drawHex(canvas, center, Hex(q + 1, r + 1), element);
+        // if (element.element.player !== null)
+          drawHexCoordinates(canvas, center, Hex(q + 1, r + 1), element);
+      });
+    });
 
-  drawHex(canvasID, center, hex, map) {
+  };
+
+  const drawHex = (canvasID, center, hex, map) => {
     for (let i = 0; i <= 5; i++) {
-      let start = this.getHexCornorCoord(center, i);
-      let end = this.getHexCornorCoord(center, i + 1);
-       if (
-        // this.state.currentHex &&
-        // map.element.player !== null &&
-        map.element.player === this.state.playerName
-      ) {
-        this.drawLine(
+      let start = getHexCornorCoord(center, i);
+      let end = getHexCornorCoord(center, i + 1);
+     checkPlayer(start,end,canvasID, center, hex, map)
+  };
+
+  function checkPlayer(start,end,canvasID, center, hex, map){
+    if (map.element.player === playerName) {
+        drawLine(
           canvasID,
           { x: start.x, y: start.y },
           { x: end.x, y: end.y },
           "black",
           1
         );
-        this.fillHex(canvasID, center, "#1D8348");
-      } 
-      else if (
-        map.element.player !== null
-      ) {
-        this.fillHex(canvasID, center, "#2ECC71 ");
-      }
-       else {
-        this.drawLine(
+        fillHex(canvasID, center, "#1D8348");
+      } else if (map.element.player !== null) {
+        fillHex(canvasID, center, "#2ECC71 ");
+      } else {
+        drawLine(
           canvasID,
           { x: start.x, y: start.y },
           { x: end.x, y: end.y },
           "blue",
           2
         );
-        this.fillHex(canvasID, center, "white");
+        fillHex(canvasID, center, "white");
       }
-      if(hex.r == this.state.currentHex.currow && hex.q == this.state.currentHex.curcol){
-        this.fillHex(canvasID, center, "red");
+      if (hex.r == currentHex.currow && hex.q == currentHex.curcol) {
+        fillHex(canvasID, center, "red");
       }
     }
   }
 
-  
-  fillHex(canvasID, center, fillColor) {
+  const fillHex = (canvasID, center, fillColor) => {
     const ctx = canvasID.getContext("2d");
     ctx.beginPath();
     for (let i = 0; i <= 5; i++) {
-      let corner = this.getHexCornorCoord(center, i);
-      if (i === 0) {
+      let corner = getHexCornorCoord(center, i);
+      if (i === 0){
         ctx.moveTo(corner.x, corner.y);
-      } else {
+      }else {
         ctx.lineTo(corner.x, corner.y);
-      }
+        }
     }
     ctx.closePath();
-
     ctx.fillStyle = fillColor;
     ctx.fill();
-  }
+  };
 
-  setCurrentHex(q, r) {
-    this.setState({ currentHex: { q, r } });
-  }
-
-  getHexCornorCoord(center, i) {
+  const getHexCornorCoord = (center, i) => {
     let angle_deg = 60 * i;
     let angle_rad = (Math.PI / 180) * angle_deg;
-    let x = center.x + this.state.hexSize * Math.cos(angle_rad);
-    let y = center.y + this.state.hexSize * Math.sin(angle_rad);
-    return this.Point(x, y);
-  }
+    let x = center.x + hexSize * Math.cos(angle_rad);
+    let y = center.y + hexSize * Math.sin(angle_rad);
+    return Point(x, y);
+  };
 
-  getHexParameters() {
-    let hexWidth = this.state.hexSize * 2;
-    let hexHeight = Math.sqrt(3) * this.state.hexSize;
+  function getHexParameters(){
+    let hexWidth = hexSize * 2;
+    let hexHeight = Math.sqrt(3) * hexSize;
     let vertDist = hexHeight;
-    let horizDist = (3 / 2) * this.state.hexSize;
+    let horizDist = (3 / 2) * hexSize;
     return { hexWidth, hexHeight, vertDist, horizDist };
-  }
+  };
 
-  hexToPixel(hex) {
-    let hexOrigin = this.state.hexOrigin;
-    let x = this.state.hexSize * ((3 / 2) * hex.q) + hexOrigin.x;
-    let y =
-      this.state.hexSize * ((Math.sqrt(3) / 2) * hex.q + Math.sqrt(3) * hex.r) +
-      hexOrigin.y;
-    return this.Point(x, y);
-  }
+  const hexToPixel = (hex) => {
+    let x = hexSize * ((3 / 2) * hex.q);
+    let y = hexSize * ((Math.sqrt(3) / 2) * hex.q + Math.sqrt(3) * hex.r);
+    return Point(x, y);
+  };
 
-  Point(x, y) {
+  const Point = (x, y) => {
     return { x: x, y: y };
-  }
+  };
 
-  Hex(q, r) {
+  const Hex = (q, r) => {
     return { q: q, r: r };
-  }
+  };
 
-  drawLine(canvasID, start, end, color, line) {
+  const drawLine = (canvasID, start, end, color, line) => {
     const ctx = canvasID.getContext("2d");
     ctx.beginPath();
     ctx.moveTo(start.x, start.y);
@@ -181,28 +144,19 @@ export default class Canvas extends React.Component {
     ctx.lineTo(end.x, end.y);
     ctx.stroke();
     ctx.closePath();
-  }
+  };
 
-  drawHexCoordinates(canvasID, center, h, map) {
+  const drawHexCoordinates = (canvasID, center, h, map) => {
     const ctx = canvasID.getContext("2d");
     ctx.fillStyle = "black";
     ctx.fillText(map.element.deposit, center.x - 5, center.y + 20);
     ctx.fillText(h.r, center.x - 10, center.y);
     ctx.fillText(h.q, center.x + 7, center.y);
-  }
-
-  useEffect = () => {
-    this.setState({
-      currentHex: { currow: this.props.currow, curcol: this.props.curcol },
-    });
   };
 
-  render() {
-    const { width, height } = this.calculateCanvasSize();
     return (
-      <div>
-        <canvas ref={this.canvasRef} width={width} height={height}></canvas>
-      </div>
-    );
-  }
+    <div>
+      <canvas ref={canvasRef} width={width} height={height}></canvas>
+    </div>
+  );
 }
--- front-end/src/components/webComponents/Map.jsx
+++ front-end/src/components/webComponents/Map.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Canvas from "./Canvas";
+import CanvasTemp from "./CanvasTemp";
 import "./styles/App.css";
 import { useAppSelector} from "../../store/hooks.ts";
 import { selectTerritory } from "../../store/Slices/territorySlice.ts";
@@ -9,23 +10,23 @@ const Map = () => {
 
   const territoryState = useAppSelector(selectTerritory);
 
+  const [currow,setCurrow] = useState(8)
+  const [curcol,setCurcol] = useState(9)
   useEffect(() => {
       // console.log(territoryState);
       // console.log(territoryState.territory[0].length);
   }, [territoryState]);
 
-  const [currow,setCurrow] = useState(4)
-  const [curcol,setCurcol] = useState(9)
+
   const mappedArray = territoryState.territory.map((innerArray, i) => {
     return innerArray.map((element, j) => {
       return {i,j,element}; 
     });
   });
+  
   return (
     <div>
       <Canvas
-        width={10000}
-        height={10000}
         mapArray = {mappedArray}
         currow = {currow}
         curcol = {curcol}
