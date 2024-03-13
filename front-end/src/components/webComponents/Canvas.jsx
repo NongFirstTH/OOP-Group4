7240b9b1be048d958e3500cb07d6d78c802ccf4c
NongFirstTH <TanabodeeChannel@gmail.com>
2024-03-13 17:06:27 +0700

    Make map can show CityCrew and CityCenter and other Plyer

--- front-end/src/components/webComponents/Canvas.jsx
+++ front-end/src/components/webComponents/Canvas.jsx
@@ -2,29 +2,25 @@ import React, { createRef, useEffect, useState } from "react";
 import "./styles/App.css";
 import { useAppSelector } from "../../store/hooks";
 import { selectConfig } from "../../store/Slices/configSlice";
-
+import { selectTerritory } from "../../store/Slices/territorySlice.ts";
 
 export default function Canvas(props) {
-    const [hexSize, setHexSize] = useState(40);
-    
-    const configState = useAppSelector(selectConfig);
-  const [rows, setRows] = useState(configState.m);
-  const [cols, setCols] = useState(configState.n);
-  const [currentHex, setCurrentHex] = useState({currow: props.currow,curcol: props.curcol,});
-  const [playerName, setPlayerName] = useState("A");
-  const canvasRef = createRef();
+    const territoryState = useAppSelector(selectTerritory);
 
-  const { width, height } = calculateCanvasSize();
+    const [hexSize, setHexSize] = useState(40);
+    const configState = useAppSelector(selectConfig);
+    const [rows, setRows] = useState(configState.m);
+    const [cols, setCols] = useState(configState.n);
+    const [playerName, setPlayerName] = useState("a");
+    const canvasRef = createRef();
+    const { width, height } = calculateCanvasSize();
+
   useEffect(() => {
       if (props.mapArray) {
-          const canvasHex = canvasRef.current;
+        const canvasHex = canvasRef.current;
           drawHexes(canvasHex);
-          console.log(props.mapArray);
-        // console.log(canvasHex);
-        // console.log(cols);
-        // console.log(canvasWidth);
     }
-  }, [props.mapArray, currentHex]);
+  }, [props.mapArray,territoryState]);
 
   function calculateCanvasSize() {
     const { hexWidth, hexHeight, vertDist, horizDist } = getHexParameters();
@@ -40,12 +36,12 @@ export default function Canvas(props) {
   };
 
   const drawHexes = (canvas) => {
-    console.log("draw")
     props.mapArray.map((innerArray, r) => {
       innerArray.map((element, q) => {
         const evenQCoord = { col: q + 1, row: r + 1 };
         const axialCoord = evenq_to_axial(evenQCoord);
         let center = hexToPixel(axialCoord);
+
         drawHex(canvas, center, Hex(q + 1, r + 1), element);
         // if (element.element.player !== null)
           drawHexCoordinates(canvas, center, Hex(q + 1, r + 1), element);
@@ -58,33 +54,41 @@ export default function Canvas(props) {
     for (let i = 0; i <= 5; i++) {
       let start = getHexCornorCoord(center, i);
       let end = getHexCornorCoord(center, i + 1);
-     checkPlayer(start,end,canvasID, center, hex, map)
+      checkPlayer(start,end,canvasID, center, hex, map)
+      checkCityCenter(canvasID, center, map)
   };
 
-  function checkPlayer(start,end,canvasID, center, hex, map){
-    if (map.element.player === playerName) {
-        drawLine(
-          canvasID,
-          { x: start.x, y: start.y },
-          { x: end.x, y: end.y },
-          "black",
-          1
-        );
+  function checkCityCenter(canvasID, center,map){
+    if (map.i === territoryState.row-1 && map.j === territoryState.col-1) {
         fillHex(canvasID, center, "#1D8348");
-      } else if (map.element.player !== null) {
-        fillHex(canvasID, center, "#2ECC71 ");
-      } else {
+      }
+  }
+
+  function checkPlayer(start,end,canvasID, center, hex, map){
+    if (map.i === territoryState.currow-1 && map.j === territoryState.curcol-1 && map.element.player === playerName) {
+        // drawLine(
+        //   canvasID,
+        //   { x: start.x, y: start.y },
+        //   { x: end.x, y: end.y },
+        //   "black",
+        //   9
+        // );
+        fillHex(canvasID, center, "#59CC8A");
+      } 
+    //   else if (map.element.player !== null) {
+      else if (props.player.p1.getRegion(map.i,map.j)!== null) {
+      fillHex(canvasID, center, props.player.p1.getColor());
+      }else if(props.player.p2.getRegion(map.i,map.j)!== null) fillHex(canvasID, center, props.player.p2.getColor());
+     
+      else {
         drawLine(
           canvasID,
           { x: start.x, y: start.y },
           { x: end.x, y: end.y },
-          "blue",
+          "#07192E",
           2
         );
-        fillHex(canvasID, center, "white");
-      }
-      if (hex.r == currentHex.currow && hex.q == currentHex.curcol) {
-        fillHex(canvasID, center, "red");
+        fillHex(canvasID, center, "#DEFFFC");
       }
     }
   }
