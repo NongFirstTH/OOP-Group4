535000f4e1b553d99b52e9795d50056024f091cb
Your Name <you@example.com>
2024-03-13 16:00:48 +0700

    make set territory set row col currow curcol

--- front-end/src/store/Slices/territorySlice.ts
+++ front-end/src/store/Slices/territorySlice.ts
@@ -28,22 +28,14 @@ export const territorySlice = createSlice({
     reducers: {
         setTerritory: (state, action: PayloadAction<TerritoryState>) => {
             state.territory = action.payload.territory;
-        },
-        setRow: (state, action: PayloadAction<TerritoryState>) => {
-        state.row = action.payload.row;
-        },
-        setCol: (state, action: PayloadAction<TerritoryState>) => {
-        state.col = action.payload.col;
-        },
-        setCurrow: (state, action: PayloadAction<TerritoryState>) => {
-        state.currow = action.payload.currow;
-        },
-        setCurcol: (state, action: PayloadAction<TerritoryState>) => {
-        state.curcol = action.payload.curcol
+            state.row = action.payload.row;
+            state.col = action.payload.col;
+            state.currow = action.payload.currow;
+            state.curcol = action.payload.curcol;
         },
     },
 });
 
-export const { setTerritory, setRow, setCol, setCurrow, setCurcol } = territorySlice.actions;
+export const { setTerritory } = territorySlice.actions;
 export default territorySlice.reducer;
 export const selectTerritory = (state: RootState) => state.territory;
