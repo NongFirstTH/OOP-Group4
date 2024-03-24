8a440d2be4321ddb18a945364d16a32d2bd2f235
Peerawich Rattanahiran <TailBeast113@gmail.com>
2024-03-24 16:29:30 +0700

    Add player Status

--- front-end/src/components/webComponents/Map.jsx
+++ front-end/src/components/webComponents/Map.jsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import Canvas from "./Canvas";
-import Slider from "./Slide"; // Import the Slider component
+import Slider from "./Slide";
+import Status from "./Status";
 
+import "../../forApp.css";
 import "./styles/App.css";
 import { useAppSelector } from "../../store/hooks.ts";
 import { selectTerritory } from "../../store/Slices/territorySlice.ts";
@@ -22,29 +24,26 @@ const Map = () => {
     setZoomLevel(newZoomLevel);
   };
 
-  const handlePan = (deltaX, deltaY) => {
-    setCenter((prevCenter) => ({
-      x: prevCenter.x + deltaX,
-      y: prevCenter.y + deltaY,
-    }));
-  };
-
   return (
-      <div className="map-container">
+    <div className="map-container">
+      <Status playerName="Player 1" budget={1000} deposit={500} />
+      <div className="canvas-slider-container">
         <div
-              style={{
-                width: "800px", // Set your desired width
-                height: "645px", // Set your desired height
-                overflow: "auto", // Enable scrolling
-                border: "1px solid #ccc", // Add a border for visualization
-              }}
-            >
-              <Canvas mapArray={mappedArray} zoomLevel={zoomLevel} />
-            </div>
+          className="canvas-container"
+          style={{
+            width: "800px", // Adjusted width considering the status bar
+            height: "585px", // Set your desired height
+            overflow: "auto", // Enable scrolling
+            border: "1px solid #ccc", // Add a border for visualization
+          }}
+        >
+          <Canvas mapArray={mappedArray} zoomLevel={zoomLevel} />
+        </div>
         <div className="slider-wrapper">
           <Slider min={0.1} max={1.5} value={zoomLevel} onChange={handleZoomChange} />
         </div>
       </div>
+    </div>
   );
 };
 
